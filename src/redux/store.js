79807d1d88540808ca3d67db03f2be99fd324a11
MuthurajaMERN <muthurajamernultrafly@gmail.com>
2024-11-12 18:49:20 +0530

    backend-connetct

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 // redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import interviewReducer from './slices/interview';
+
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    interview:interviewReducer,
   },
 });
 
